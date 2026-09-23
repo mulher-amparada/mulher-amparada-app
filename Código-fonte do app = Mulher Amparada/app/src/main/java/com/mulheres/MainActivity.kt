3ebd3f42cbf3cb0feda15a4beb0cd52f804050df
Mulher amparada <mulheramparada@gmail.com>
2026-09-23 18:57:13 -0300

    Atualizar o MainActivity.kt

--- Código-fonte do app = Mulher Amparada/app/src/main/java/com/mulheres/MainActivity.kt
+++ Código-fonte do app = Mulher Amparada/app/src/main/java/com/mulheres/MainActivity.kt
@@ -2112,7 +2112,7 @@ fun solicitarAdministradorNovamente() {
             MaterialAlertDialogBuilder(this)
                 .setTitle("Administrador necessário")
                 .setMessage(
-                    "Sem essa permissão, o recurso de bloqueio por barulho não funcionará."
+                    "Sem essa permissão, recursos que bloqueiam a tela não funcionarão (como por exemplo o bloqueio por barulho ou bloquear a tela pela área protegida)"
                 )
                 .setNegativeButton("Cancelar", null)
                 .setPositiveButton("Tentar novamente") { _, _ ->

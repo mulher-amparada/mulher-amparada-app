f645190ddd338fbca1aa8172977dd2581b5a325f
Mulher amparada <mulheramparada@gmail.com>
2026-09-26 08:57:51 -0300

    Atualizar o AssistenteActivity.kt

--- Código-fonte do app = Mulher Amparada/app/src/main/java/com/mulheres/AssistenteActivity.kt
+++ Código-fonte do app = Mulher Amparada/app/src/main/java/com/mulheres/AssistenteActivity.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.core.LinearEasing
-port androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.rememberInfiniteTransition
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
